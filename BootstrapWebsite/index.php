15f17d676d396e11504aba5e82c27a0044cd7529
CodeSwagGuy <codeswag@gmx.co.uk>
2014-10-13 20:37:37 +0100

    Added a significant portion of the website

--- BootstrapWebsite/index.php
+++ BootstrapWebsite/index.php
@@ -30,9 +30,10 @@
             <div class="inner">
               <h3 class="masthead-brand">CreativeCode+</h3>
               <ul class="nav masthead-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Features</a></li>
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
+                <li><a href="login1.php">Login</a></li>
               </ul>
             </div>
           </div>
@@ -60,6 +61,20 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
+      
+    <!-- Google Analytics -->
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-55594491-1', 'auto');
+    ga('send', 'pageview');
+
+    </script>
+    <!-- Google Analytics -->
+      
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </body>
